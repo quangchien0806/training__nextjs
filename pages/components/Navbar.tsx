f18bcef716b0f302c8e52d079d6267d5e4533339
Your Name <quangchien220399@gmail.com>
2022-06-29 14:24:53 +0700

    first commit

---
+++ pages/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center text-4xl h-24 leading-[6rem]">
+      <img className="w-20 h-20 mr-4" src="../spotify-brands.svg" alt="" />
+      Spotify
+    </div>
+  );
+}
